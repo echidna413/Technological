6041324c64c32f047888dcdf4754da7b1c1b9432
Andrej Petrov <apetrov-radnaadr@yandex.ru>
2019-06-05 01:33:00 +0400

    Добавлены начальные данные по справочникам

--- Tadb.DAL/Migrations/Configuration.cs
+++ Tadb.DAL/Migrations/Configuration.cs
@@ -31,8 +31,15 @@ namespace Tadb.DAL.Migrations
             SeedRoles(context);
             SeedCompanies(context);
             SeedEmployees(context);
-            context.SaveChanges();
 
+            SeedDetails(context);
+
+            SeedEquipmentCatalog(context);
+            SeedSurfaceCatalog(context);
+            SeedFixtureCatalog(context);
+            SeedProcessCatalog(context);
+
+            context.SaveChanges();
         }
 
         private void SeedEmployees(MachineDbContext context)
@@ -67,6 +74,7 @@ namespace Tadb.DAL.Migrations
             context.Companies.AddOrUpdate(devCompany);
 
             #region Место для добавление компаний
+
             //Company[] companies = new Company[]
             //{
             //    new Company()
@@ -95,14 +103,15 @@ namespace Tadb.DAL.Migrations
             //    }
             //};
 
-            //context.Companies.AddOrUpdate(companies); 
-            #endregion
+            //context.Companies.AddOrUpdate(companies);
+
+            #endregion Место для добавление компаний
         }
 
         private static void SeedRoles(MachineDbContext context)
         {
             Role[] roles = new[]
-                        {
+            {
                 new Role
                 {
                     id_role = 1,
@@ -133,6 +142,150 @@ namespace Tadb.DAL.Migrations
             };
 
             context.Roles.AddOrUpdate(roles);
+        }
+
+        private static void SeedDetails(MachineDbContext context)
+        {
+            DDescription[] details = new[]{
+                new DDescription()
+                {
+                    detail_id = 1,
+                    id_employee = 1,
+                    name_detail = "Гайка с конической шестерней",
+                    mark_material = "38М",
+                    size_production = 200,
+                    size_transmission = 80,
+                    central_hole = true,
+                }
+            };
+
+            context.DDescriptions.AddOrUpdate(details);
+        }
+
+        private static void SeedSurfaceCatalog(MachineDbContext context)
+        {
+            SurfaceCatalog[] surfaceCatalog = new[]
+            {
+                new SurfaceCatalog()
+                {
+                    surface_code = 9111,
+                    name = "Внутренняя цилиндрическая"
+                },
+                new SurfaceCatalog()
+                {
+                    surface_code = 131,
+                    name = "Выточка наружная"
+                },
+                new SurfaceCatalog()
+                {
+                    surface_code = 9121,
+                    name = "Канавка внутренняя"
+                },
+                new SurfaceCatalog()
+                {
+                    surface_code = 9312,
+                    name = "Торец на цилиндрическом отверстии параллельном оси"
+                },
+                new SurfaceCatalog()
+                {
+                    surface_code = 311,
+                    name = "Торец наружный"
+                }
+            };
+
+            context.CodesSurfaces.AddOrUpdate(surfaceCatalog);
+        }
+
+        private static void SeedEquipmentCatalog(MachineDbContext context)
+        {
+            EquipmentCatalog[] equipmentCatalog = new[]
+            {
+                new EquipmentCatalog()
+                {
+                    equipment_code = 1,
+                    model = "16К20",
+                    description = "Токарно-винторезный"
+                },
+                new EquipmentCatalog()
+                {
+                    equipment_code = 2,
+                    model = "16Л21",
+                    description = "Токарный"
+                },
+                new EquipmentCatalog()
+                {
+                    equipment_code = 3,
+                    model = "16Д22",
+                    description = "Винторезный"
+                },
+            };
+
+            context.EquipmentCatalogs.AddOrUpdate(equipmentCatalog);
+        }
+
+        private static void SeedProcessCatalog(MachineDbContext context)
+        {
+            ProcessCatalog[] processCatalog = new[]
+            {
+                new ProcessCatalog()
+                {
+                    process_code = 1,
+                    description = "Точение черновое"
+                },
+                new ProcessCatalog()
+                {
+                    process_code = 2,
+                    description = "Точение получистовое"
+                },
+                new ProcessCatalog()
+                {
+                    process_code = 3,
+                    description = "Точение чистовое"
+                },
+                new ProcessCatalog()
+                {
+                    process_code = 4,
+                    description = "Точение тонкое"
+                },
+                new ProcessCatalog()
+                {
+                    process_code = 5,
+                    description = "Шлифование предварительное"
+                },
+                new ProcessCatalog()
+                {
+                    process_code = 6,
+                    description = "Шлифование чистовое"
+                },
+                new ProcessCatalog()
+                {
+                    process_code = 17,
+                    description = "Фрезерование"
+                },
+            };
+
+            context.ProcessCatalogs.AddOrUpdate(processCatalog);
+        }
+
+        private static void SeedFixtureCatalog(MachineDbContext context)
+        {
+            FixtureCatalog[] fixtureCatalog = new[]
+            {
+                new FixtureCatalog()
+                {
+                    fixture_code = 1,
+                    fixture_value = "7100-0007",
+                    description = "Трехкулачковый патрон"
+                },
+                new FixtureCatalog()
+                {
+                    fixture_code = 2,
+                    fixture_value = "7100-0008",
+                    description = "Патрон"
+                },
+            };
+
+            context.FixtureCatalogs.AddOrUpdate(fixtureCatalog);
         }
     }
 }
